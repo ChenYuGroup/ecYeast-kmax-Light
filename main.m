f983dfbc2682967abbaf40db7e92523708657dcb
Yu Chen <yuchen.scholar@gmail.com>
2025-03-31 11:14:10 +0800

    Create main.m

---
+++ main.m
@@ -0,0 +1,23 @@
+addpath(genpath('../cobratoolbox'));
+addpath(genpath('../Yeast_kapp'));
+addpath(genpath('../Amino_acid/'));
+initCobraToolbox;
+
+load('GEM-yeast-split.mat'); % from /GitHub/Amino_acid/Models
+
+enzymedata = collectkcats(model_split.rxns(~ismember(model_split.rules,{''})),model_split,'saccharomyces cerevisiae');
+enzymedata.kcat(enzymedata.kcat < quantile(enzymedata.kcat,0.1,1)) = quantile(enzymedata.kcat,0.1,1);
+enzymedata = updateYeastEnzyme(enzymedata,model_split);
+
+model = convertModel(model_split,enzymedata);
+
+model = setYeastMedia(model);
+model = changeRxnBounds(model,'r_1714',-1000,'l');
+model = changeRxnBounds(model,'prot_cost_exchange',0.1,'u');
+
+model = changeObjective(model,'r_2111');
+
+sol = optimizeCbModel(model);
+sol.f
+
+save('model.mat',"model"); % the model saved in the path.
